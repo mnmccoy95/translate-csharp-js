0043e8aaba85030586abd2b1127b9f90357635d5
mnmccoy95 <mnmccoy95@gmail.com>
2020-11-16 11:19:34 -0600

    fizzbuzz complete

---
+++ fizzbuzz/csharp/fizzbuzz/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace fizzbuzz
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for(int n=0; n<=100; n++)
+            {
+                if(n % 15 == 0)
+                {
+                    Console.WriteLine("fizzbuzz");
+                }
+                if(n % 5 == 0)
+                {
+                    Console.WriteLine("buzz");
+                }
+                if(n % 3 == 0)
+                {
+                    Console.WriteLine("fizz");
+                }
+                else
+                {
+                    Console.WriteLine(n);
+                }
+            }
+        }
+    }
+}
